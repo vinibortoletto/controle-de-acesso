68f498b21c64dbbb4554f105644c97c1737599ae
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-02 16:51:24 -0300

    feat: finish interaction

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,5 +1,6 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -10,7 +11,7 @@ public class Principal {
   /**
    * Método displayOptions.
    */
-  public static void displayOptions() {
+  private static void displayOptions() {
     System.out.println("Entre com o número correspondente à opção desejada:");
     System.out.println("1 - Acessar o estabelecimento");
     System.out.println("2 - Finalizar sistema e mostrar relatório");
@@ -19,7 +20,7 @@ public class Principal {
   /**
    * Método printMessage.
    */
-  public static void printMessage(int age) {
+  private static void printMessage(int age) {
     String minorMessage = "Pessoa cliente menor de idade, catraca liberada!";
     String adultMessage = "Pessoa adulta, catraca liberada!";
     String oldMessage = "Pessoa adulta a partir de 50, catraca liberada!";
@@ -34,6 +35,27 @@ public class Principal {
   }
 
   /**
+   * Método getNumberOfClientsBasedOnAge.
+   */
+  private static int getNumberOfClientsBasedOnAge(ArrayList<Integer> ageList, String filter) {
+    ArrayList<Integer> filteredAgeList = new ArrayList<Integer>();
+
+    for (int i = 0; i < ageList.size(); i++) {
+      int age = (int) ageList.get(i);
+
+      if (filter == "minor" && age < 18) {
+        filteredAgeList.add(age);
+      } else if (filter == "adult" && age >= 18 && age < 50) {
+        filteredAgeList.add(age);
+      } else if (filter == "old" && age >= 50) {
+        filteredAgeList.add(age);
+      }
+    }
+
+    return filteredAgeList.size();
+  }
+
+  /**
    * Método principal.
    */
   public static void main(String[] args) {
@@ -43,18 +65,41 @@ public class Principal {
     displayOptions();
     int option = scanner.nextInt();
 
-    if (option == 2) {
-      System.out.println(ageList);
+    if (option != 1 && option != 2) {
+      System.out.println("Entre com uma opção válida");
+    } else if (option == 1) {
+
+      while (option != 2) {
+        System.out.println("Entre com a sua idade:");
+        int age = scanner.nextInt();
+        ageList.add(age);
+        printMessage(age);
+
+        displayOptions();
+        option = scanner.nextInt();
+      }
     }
 
-    while (option != 2) {
-      System.out.println("Entre com a sua idade:");
-      int age = scanner.nextInt();
-      ageList.add(age);
-      printMessage(age);
+    if (option == 2) {
+      int totalClient = ageList.size();
+      int minor = getNumberOfClientsBasedOnAge(ageList, "minor");
+      int adult = getNumberOfClientsBasedOnAge(ageList, "adult");
+      int old = getNumberOfClientsBasedOnAge(ageList, "old");
+      DecimalFormat decimalFormat = new DecimalFormat("0.0%");
 
-      displayOptions();
-      option = scanner.nextInt();
+      float minorPercentage = (float) minor / totalClient;
+      float adultPercentage = (float) adult / totalClient;
+      float oldPercentage = (float) old / totalClient;
+
+      System.out.println("----- Quantidade -----");
+      System.out.println("menores: " + minor);
+      System.out.println("adultas: " + adult);
+      System.out.println("a partir de 50: " + old);
+
+      System.out.println("----- Percentual -----");
+      System.out.println("menores: " + decimalFormat.format(minorPercentage));
+      System.out.println("adultas: " + decimalFormat.format(adultPercentage));
+      System.out.println("a partir de 50: " + decimalFormat.format(oldPercentage));
     }
 
     scanner.close();
