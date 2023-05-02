24cf23dd09217c5810fd61d72a59a032a4e8cfb2
Vinicius Bortoletto <ovinibortoletto@gmail.com>
2023-05-02 15:47:38 -0300

    feat: console intereaction

--- src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
+++ src/main/java/com/trybe/acc/java/controledeacesso/Principal.java
@@ -1,11 +1,62 @@
 package com.trybe.acc.java.controledeacesso;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Classe Principal.
+ */
 public class Principal {
+  /**
+   * Método displayOptions.
+   */
+  public static void displayOptions() {
+    System.out.println("Entre com o número correspondente à opção desejada:");
+    System.out.println("1 - Acessar o estabelecimento");
+    System.out.println("2 - Finalizar sistema e mostrar relatório");
+  }
+
+  /**
+   * Método printMessage.
+   */
+  public static void printMessage(int age) {
+    String minorMessage = "Pessoa cliente menor de idade, catraca liberada!";
+    String adultMessage = "Pessoa adulta, catraca liberada!";
+    String oldMessage = "Pessoa adulta a partir de 50, catraca liberada!";
+
+    if (age < 18) {
+      System.out.println(minorMessage);
+    } else if (age >= 18 && age < 50) {
+      System.out.println(adultMessage);
+    } else {
+      System.out.println(oldMessage);
+    }
+  }
 
   /**
    * Método principal.
    */
   public static void main(String[] args) {
-    // ESCREVA SEU CÓDIGO AQUI
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Integer> ageList = new ArrayList<Integer>();
+
+    displayOptions();
+    int option = scanner.nextInt();
+
+    if (option == 2) {
+      System.out.println(ageList);
+    }
+
+    while (option != 2) {
+      System.out.println("Entre com a sua idade:");
+      int age = scanner.nextInt();
+      ageList.add(age);
+      printMessage(age);
+
+      displayOptions();
+      option = scanner.nextInt();
+    }
+
+    scanner.close();
   }
 }
